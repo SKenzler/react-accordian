5eba4dd5ae2d4740763ab1c35c1939e6e1c90fce
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-03-24 18:32:04 +0800

    add multiple selection functionaly

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -3,13 +3,34 @@ import data from "./data";
 
 export const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [multiSelection, setMultiSelection] = useState(false);
+  const [userSelections, setUserSelections] = useState([]);
 
   const handleSingleSelection = (currentId) => {
     setSelected(currentId === selected ? null : currentId);
   };
 
+  const handleMutliSelection = (currentId) => {
+    let copySelections = [...userSelections];
+    const findCurrentIdIndex = copySelections.indexOf(currentId);
+
+    if (findCurrentIdIndex === -1) copySelections.push(currentId);
+    else copySelections.splice(findCurrentIdIndex, 1);
+
+    setUserSelections(copySelections);
+  };
+
   return (
-    <div className="h-full max-h-screen flex items-center justify-center bg-slate-900">
+    <div className="h-full max-h-screen flex flex-col items-center justify-center bg-slate-900">
+      <button
+        onClick={() => setMultiSelection(!multiSelection)}
+        type="button"
+        className="bg-sky-600 text-sm text-white text-center font-bold rounded-md px-8 py-2 mt-4 hover:bg-sky-800 cursor-pointer"
+      >
+        {multiSelection
+          ? "Enable Single Selection"
+          : "Enable Multiple Selections"}
+      </button>
       <div>
         {data && data.length > 0 ? (
           data.map((dataItem) => (
@@ -19,28 +40,48 @@ export const Accordian = () => {
               </div>
               <button
                 type="button"
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  multiSelection
+                    ? () => handleMutliSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="flex justify-center content-center font-bold hover:text-slate-900 text-white text-lg rounded-full p-1 mt-2 cursor-pointer"
               >
                 {selected === dataItem.id ? "-" : "+"}
               </button>
-
-              {selected === dataItem.id ? (
-                <div className="text-slate-900 text-base text-center">
-                  <span className="font-bold px-2">Year of Birth:</span>
-                  {dataItem.birth_year}
-                  <span className="font-bold px-2">Height:</span>
-                  {dataItem.height}
-                  <span className="font-bold px-2">Mass:</span>
-                  {dataItem.mass}
-                  <span className="font-bold px-2">Hair Colour:</span>
-                  {dataItem.hair_color}
-                  <span className="font-bold px-2">Skin Colour:</span>
-                  {dataItem.skin_color}
-                  <span className="font-bold px-2">Eye Colour:</span>
-                  {dataItem.eye_color}
-                </div>
-              ) : null}
+              {multiSelection
+                ? userSelections.indexOf(dataItem.id) !== -1 && (
+                    <div className="text-slate-900 text-base text-center">
+                      <span className="font-bold px-2">Year of Birth:</span>
+                      {dataItem.birth_year}
+                      <span className="font-bold px-2">Height:</span>
+                      {dataItem.height}
+                      <span className="font-bold px-2">Mass:</span>
+                      {dataItem.mass}
+                      <span className="font-bold px-2">Hair Colour:</span>
+                      {dataItem.hair_color}
+                      <span className="font-bold px-2">Skin Colour:</span>
+                      {dataItem.skin_color}
+                      <span className="font-bold px-2">Eye Colour:</span>
+                      {dataItem.eye_color}
+                    </div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="text-slate-900 text-base text-center">
+                      <span className="font-bold px-2">Year of Birth:</span>
+                      {dataItem.birth_year}
+                      <span className="font-bold px-2">Height:</span>
+                      {dataItem.height}
+                      <span className="font-bold px-2">Mass:</span>
+                      {dataItem.mass}
+                      <span className="font-bold px-2">Hair Colour:</span>
+                      {dataItem.hair_color}
+                      <span className="font-bold px-2">Skin Colour:</span>
+                      {dataItem.skin_color}
+                      <span className="font-bold px-2">Eye Colour:</span>
+                      {dataItem.eye_color}
+                    </div>
+                  )}
             </div>
           ))
         ) : (
