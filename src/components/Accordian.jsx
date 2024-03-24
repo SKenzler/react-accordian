1d0ac1b6172930252005a912f8e33f4c08a9c86d
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-03-24 16:24:38 +0800

    add additional content to cards and toggle button betweeb + and -

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -9,12 +9,12 @@ export const Accordian = () => {
   };
 
   return (
-    <div className="h-full bg-slate-900 flex justify-center content-center">
+    <div className="h-full max-h-screen flex items-center justify-center bg-slate-900">
       <div>
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="bg-yellow-400 flex flex-col items-center justify-center rounded-md shadow-lg shadow-white/20 px-20 py-2 m-3">
-              <div className="flex text-slate-900 font-semibold text-xl/8">
+            <div className="flex flex-col items-center justify-center bg-yellow-400  rounded-md shadow-lg shadow-white/20 px-10 py-2 m-3">
+              <div className=" text-slate-900 font-semibold text-xl/8">
                 {dataItem.name}
               </div>
               <button
@@ -22,10 +22,24 @@ export const Accordian = () => {
                 onClick={() => handleSingleSelection(dataItem.id)}
                 className="flex justify-center content-center font-bold hover:text-slate-900 text-white text-lg rounded-full p-1 mt-2 cursor-pointer"
               >
-                +
+                {selected === dataItem.id ? "-" : "+"}
               </button>
+
               {selected === dataItem.id ? (
-                <div>Year of Birth: {dataItem.birth_year}</div>
+                <div className="text-slate-900 text-base text-center">
+                  <span className="font-bold px-2">Year of Birth:</span>
+                  {dataItem.birth_year}
+                  <span className="font-bold px-2">Height:</span>
+                  {dataItem.height}
+                  <span className="font-bold px-2">Mass:</span>
+                  {dataItem.mass}
+                  <span className="font-bold px-2">Hair Colour:</span>
+                  {dataItem.hair_color}
+                  <span className="font-bold px-2">Skin Colour:</span>
+                  {dataItem.skin_color}
+                  <span className="font-bold px-2">Eye Colour:</span>
+                  {dataItem.eye_color}
+                </div>
               ) : null}
             </div>
           ))
